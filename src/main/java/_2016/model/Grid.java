598f6d096ff63e925b438929087bef2184c39ee9
Jean-Eudes <jeudes7@gmail.com>
2016-02-11 19:04:02 +0100

    add grid model

---
+++ src/main/java/_2016/model/Grid.java
@@ -0,0 +1,11 @@
+package _2016.model;
+
+public class Grid {
+
+    public final int rows, columns;
+
+    public Grid(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+    }
+}
