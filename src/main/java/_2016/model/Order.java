d94b9e8133d203e49bb5e5768eaa7694668b9869
ibeauvais <ivan.beauvais@gmail.com>
2016-02-11 19:16:28 +0100

    update Order

--- src/main/java/_2016/model/Order.java
+++ src/main/java/_2016/model/Order.java
@@ -1,4 +1,14 @@
 package _2016.model;
 
+import java.util.List;
+
 public class Order {
+
+    public final Position position;
+    public final List<Item> expecteditems;
+
+    public Order(Position position, List<Item> expecteditems) {
+        this.position = position;
+        this.expecteditems = expecteditems;
+    }
 }
