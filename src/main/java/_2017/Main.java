4580d5134a386aa7cd9991d806d2fb395118cb8f
julien <girardot.jul@gmail.com>
2017-02-23 19:57:18 +0100

    main

--- src/main/java/_2017/Main.java
+++ src/main/java/_2017/Main.java
@@ -9,10 +9,14 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         InputReader inputReader = new InputReader();
-        String[] fileNames = {"file.in"};
+        //String[] fileNames = {"kittens.in", "me_at_the_zoo.in", "trending_today.in", "videos_worth_spreading.in"};
+        String[] fileNames = {"kittens.in"};
+
+        final Processor processor = new SimpleProcessor();
         for (String fileName : fileNames) {
             World world = inputReader.parse("/" + fileName);
-
+            processor.process(world);
+            world.write(fileName.replace(".in", ".out"));
         }
 
     }
